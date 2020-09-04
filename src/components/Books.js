f8548be630670dacfc7af1813e983cd0b8ef656c
Eliott Davidson <ulthos@gmail.com>
2020-09-04 13:25:26 -0700

    Changed Books.js to query database instead of googleapis

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,23 +5,29 @@ import { Link } from 'react-router-dom';
 export default function Books() {
     const [books, setBooks] = useState([]);
     const [error, setError] = useState(null);
-    const [searchParam, setSearchParam] = useState()
+    const [searchTitle, setSearchTitle] = useState("")
+    const [searchAuthor, setSearchAuthor] = useState("")
 
-    const handleSearch = (e) => {
+    const handleTitle = (e) => {
         console.log(e.target.value)
-        setSearchParam(e.target.value)
+        setSearchTitle(e.target.value)
+    }    
+    const handleAuthor = (e) => {
+        console.log(e.target.value)
+        setSearchAuthor(e.target.value)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.get(`https://www.googleapis.com/books/v1/volumes?key=${process.env.REACT_APP_API_KEY}&q=${searchParam}`)
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/books?title=${encodeURIComponent(searchTitle)}&author=${encodeURIComponent(searchAuthor)}`)
+        //axios.get(`https://www.googleapis.com/books/v1/volumes?key=${process.env.REACT_APP_API_KEY}&q=${searchParam}`)
             .then(response => {
                 // check the response is good
                 if (response.status === 200) {
                     // set books equal to 
                     console.log("Hitting the search data now", response.data)
-                    console.log("data.items", response.data.items)
-                    setBooks(response.data.items) 
+                    console.log("data.books", response.data.books)
+                    setBooks(response.data.books) 
                 } else {
                     setError(response.statusText)
                 }
@@ -36,21 +42,22 @@ export default function Books() {
     const handleClear = (e) => {
         e.preventDefault()
         setBooks([])
-        setSearchParam()
+        setSearchTitle("")
+        setSearchAuthor("")
     }
 
     let displayBooks = books.map((book, key) => {
-        let authors = book.volumeInfo.authors.map((author, key) => {
-            return (
-                <span> {author} &nbsp;&nbsp; </span>
-                )
-        })
+        //let authors = book.author.map((author, key) => {
+        //    return (
+        //        <span> {author} &nbsp;&nbsp; </span>
+        //        )
+        //})
         return (
             <div>
-                <Link to={`/book/${book.id}?title=${book.volumeInfo.title}&author=${book.volumeInfo.authors[0]}`}>
-                Title: {book.volumeInfo.title} &nbsp;&nbsp;
-                Authors: {authors} &nbsp;&nbsp;
-                Published: {book.volumeInfo.publishedDate} 
+                <Link to={`/book/${book._id}`}>
+                Title: {book.title} &nbsp;&nbsp;
+                Authors: {book.author} &nbsp;&nbsp;
+                {/*Published: {book.volumeInfo.publishedDate} */}
                 </Link>
             </div>
         )
@@ -62,10 +69,15 @@ export default function Books() {
             <form onSubmit={handleSubmit} >
                 <br></br>
                 <div>
-                <label>
-                    Search for books:
-                    <input type="search" placeholder="Search Parameter" onChange={handleSearch} />
-                </label>
+                    <p>Search for books:</p>
+                    <label>
+                        title:
+                        <input type="search" placeholder="Search Parameter" onChange={handleTitle} />
+                    </label>
+                    <label>
+                        author:
+                        <input type="search" placeholder="Search Parameter" onChange={handleAuthor} />
+                    </label>
                 </div>
                 <br></br>
                 <input type="submit" value="Submit" />
