09506ade6091c8f311ffec51d254dfa0102561af
Eliott Davidson <ulthos@gmail.com>
2020-09-07 09:45:38 -0700

    Fixed typo in ReaderExperience state

--- src/components/ReaderExperience.js
+++ src/components/ReaderExperience.js
@@ -19,7 +19,7 @@ export default function ReaderExperience({ currentUser }) {
     let [ author, setAuthor ] = useState("");
     let [ description, setDescription ] = useState("");
     let [ shouldRedirect, setShouldRedirect ] = useState(false);
-    let { readerExperienceId, setReaderExperienceId } = useState("");
+    let [ readerExperienceId, setReaderExperienceId ] = useState("");
 
     const handleRating = (e) => {
         setRating(e.target.value)
@@ -48,7 +48,7 @@ export default function ReaderExperience({ currentUser }) {
         if (dateStarted) readerExperienceData.date_started = dateStarted;
         if (dateFinished) readerExperienceData.date_finished = dateFinished;
         console.log(`Sending update to backend: ${JSON.stringify(readerExperienceData)}`)
-        Axios.put(`${process.env.REACT_APP_SERVER_URL}readerexperiences/${readerExperienceId}}`, readerExperienceData)
+        Axios.put(`${process.env.REACT_APP_SERVER_URL}/readerexperiences/${readerExperienceId}}`, readerExperienceData)
             .then(res => {
                 console.log(`Update response from backend: ${JSON.stringify(res)}`)
             })
@@ -64,7 +64,7 @@ export default function ReaderExperience({ currentUser }) {
 
     useEffect(() => {
         if (!title){    // only make db call if necessary
-            Axios.get(`${process.env.REACT_APP_SERVER_URL}readerexperiences?book=${queryParams.get("book")}&user=${currentUser.id}`, )
+            Axios.get(`${process.env.REACT_APP_SERVER_URL}/readerexperiences?book=${queryParams.get("book")}&user=${currentUser.id}`, )
                 .then(response => {
                     console.log(`response: ${JSON.stringify(response)}`);
                     if (response.status === 200){
